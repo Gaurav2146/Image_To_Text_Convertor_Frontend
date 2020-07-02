0cb6d5775989e2f02cd7c6c878d74506e1ad6fb4
Gaurav2146 <61244204+Gaurav2146@users.noreply.github.com>
2020-07-02 22:55:32 +0530

    Updates

---
+++ precache-manifest.f634b8f68f02fc2e13a88b6fa709b203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fe1ada9a21620315fed9561622ad35",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "9abe9cf6cebc8d8335ec",
+    "url": "/hello/static/css/main.bde42325.chunk.css"
+  },
+  {
+    "revision": "79ca307d1f2547c6cf9d",
+    "url": "/hello/static/js/2.b61c02c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hello/static/js/2.b61c02c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9abe9cf6cebc8d8335ec",
+    "url": "/hello/static/js/main.5bbe64b1.chunk.js"
+  },
+  {
+    "revision": "543a3730c717a6d3edaf",
+    "url": "/hello/static/js/runtime-main.eda9fcde.js"
+  }
+]);
